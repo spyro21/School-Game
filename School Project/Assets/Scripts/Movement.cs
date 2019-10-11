a50507ee8e355a71a5486fa1ec3e2af5a592a5c7
iamorrow <56452518+iamorrow@users.noreply.github.com>
2019-10-11 14:49:39 -0500

    added sounds to sans
    hehe pbttt

--- School Project/Assets/Scripts/Movement.cs
+++ School Project/Assets/Scripts/Movement.cs
@@ -6,18 +6,28 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public AudioClip walkNoise;
+    public AudioSource NoiseSource;
 
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
 
     Vector2 movement;
-
+    private void Start()
+    {
+        NoiseSource.clip = walkNoise;
+    }
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            NoiseSource.Play();
+        }
+        
     }
 
     private void FixedUpdate()
---
+++ School Project/Assets/Scripts/audio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") == 0)
+        {
+            MusicSource.Play();
+        }
+        else if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            MusicSource.Play();
+        }
+    }
+}
